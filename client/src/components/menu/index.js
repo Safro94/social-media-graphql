6503729e27cbb284391ff52bde6f83e4497d7144
Matias Safranchik <msafranchik@TEC32M>
2020-11-11 12:03:57 -0300

    add menu routes and styling

--- client/src/components/menu/index.js
+++ client/src/components/menu/index.js
@@ -1,18 +1,38 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 import { HOME, LOGIN, REGISTER } from 'constants/routes';
 
 import classes from './index.module.css';
 
 export default () => {
+  const location = useLocation();
+  const [active, setActive] = useState(location.pathname);
+
   return (
     <div className={classes.container}>
-      <div>
-        <Link to={HOME}>Home</Link>
-      </div>
+      <Link
+        className={`${classes.link} ${active === HOME && classes.active}`}
+        to={HOME}
+        onClick={() => setActive(HOME)}
+      >
+        Home
+      </Link>
       <div className={classes.rightLinks}>
-        <Link to={LOGIN}>Login</Link>
-        <Link to={REGISTER}>Register</Link>
+        <Link
+          className={`${classes.link} ${active === LOGIN && classes.active}`}
+          to={LOGIN}
+          onClick={() => setActive(LOGIN)}
+        >
+          Login
+        </Link>
+        <Link
+          className={`${classes.link} ${active === REGISTER && classes.active}`}
+          to={REGISTER}
+          onClick={() => setActive(REGISTER)}
+        >
+          Register
+        </Link>
       </div>
     </div>
   );
