2ee9244679b64ae98e943a5bc5a6a306edc9e38d
Matias Safranchik <msafranchik@TEC32M>
2020-11-13 09:58:25 -0300

    add delete button

--- client/src/components/postCard/index.js
+++ client/src/components/postCard/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaHeart, FaComment } from 'react-icons/fa';
+import { FaHeart, FaComment, FaTrash } from 'react-icons/fa';
 import { IconContext } from 'react-icons';
 import moment from 'moment';
+
+import { useAuth } from 'hooks/auth';
 
 import classes from './index.module.css';
 
 export default ({
   post: { body, id, username, createdAt, likeCount, commentCount, likes },
 }) => {
+  const { user } = useAuth();
   const handleLikes = () => console.log('like');
-  const handleComments = () => console.log('comment');
 
   return (
     <div className={classes.container}>
@@ -29,41 +31,61 @@ export default ({
         </div>
       </div>
       <div className={classes.buttonsContainer}>
-        <div className={classes.buttonContainer}>
-          <button
-            className={`${classes.button} ${classes.likeButton}`}
-            onClick={handleLikes}
-          >
-            <IconContext.Provider
-              value={{ color: 'turquoise', className: classes.icon }}
+        <div>
+          <div className={classes.buttonContainer}>
+            <button
+              className={`${classes.button} ${classes.likeButton}`}
+              onClick={handleLikes}
             >
-              <div className={classes.iconContainer}>
-                <FaHeart />
-              </div>
-            </IconContext.Provider>
-          </button>
-          <div className={`${classes.count} ${classes.likeCount}`}>
-            <span>{likeCount}</span>
+              <IconContext.Provider
+                value={{ color: 'turquoise', className: classes.icon }}
+              >
+                <div className={classes.iconContainer}>
+                  <FaHeart />
+                </div>
+              </IconContext.Provider>
+            </button>
+            <div className={`${classes.count} ${classes.likeCount}`}>
+              <span>{likeCount}</span>
+            </div>
           </div>
-        </div>
 
-        <div className={classes.buttonContainer}>
-          <button
-            className={`${classes.button} ${classes.commentButton}`}
-            onClick={handleComments}
-          >
-            <IconContext.Provider
-              value={{ color: '#3d3dd0', className: classes.icon }}
+          <div className={classes.buttonContainer}>
+            <Link
+              className={`${classes.button} ${classes.commentButton}`}
+              to={`/posts/${id}`}
             >
-              <div className={classes.iconContainer}>
-                <FaComment />
-              </div>
-            </IconContext.Provider>
-          </button>
-          <div className={`${classes.count} ${classes.commentCount}`}>
-            <span>{commentCount}</span>
+              <IconContext.Provider
+                value={{ color: '#3d3dd0', className: classes.icon }}
+              >
+                <div className={classes.iconContainer}>
+                  <FaComment />
+                </div>
+              </IconContext.Provider>
+            </Link>
+            <div className={`${classes.count} ${classes.commentCount}`}>
+              <span>{commentCount}</span>
+            </div>
           </div>
         </div>
+        {user && user.username === username && (
+          <div
+            className={`${classes.buttonContainer} ${classes.trashButtonContainer}`}
+          >
+            <button
+              className={`${classes.button} ${classes.trashButton}`}
+              onClick={handleLikes}
+            >
+              <IconContext.Provider
+                value={{ color: 'white', className: classes.icon }}
+              >
+                <div className={classes.iconContainer}>
+                  <FaTrash />
+                </div>
+              </IconContext.Provider>
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
